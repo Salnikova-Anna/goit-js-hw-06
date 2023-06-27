11fcc1caffc2ba143e3253591c7978e67e5f7390
Salnikova-Anna <anna_val@ukr.net>
2023-06-27 12:23:10 +0300

    task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsListRef = document.getElementById("ingredients");
+
+const ingredientsItems = ingredients.map((ingredient) => {
+  const ingredientsItem = document.createElement("li");
+  ingredientsItem.textContent = ingredient;
+  ingredientsItem.classList.add("item");
+
+  return ingredientsItem;
+});
+
+ingredientsListRef.append(...ingredientsItems);
