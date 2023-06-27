78c139cc30b9a9ba1165a5aea89b53991794c162
Salnikova-Anna <anna_val@ukr.net>
2023-06-27 11:26:13 +0300

    Add prettierrc.json

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -5,7 +5,7 @@ const categoriesItemsArray = [...categoriesListRef.children];
 console.log(`Number of categories: ${categoriesItemsArray.length}`);
 
 categoriesItemsArray.forEach((category) => {
-  console.log(`Category: ${category.firstElementChild.textContent}`);
+  console.log(`\n Category: ${category.firstElementChild.textContent}`);
 
   console.log(`Elements: ${category.lastElementChild.children.length}`);
 });
