cd8ea961bc9917c07078f89eb5a44c45e31c0aba
Salnikova-Anna <anna_val@ukr.net>
2023-06-29 14:40:07 +0300

    Complete task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorRef = document.querySelector(".color");
+const buttonRef = document.querySelector(".change-color");
+
+buttonRef.addEventListener("click", buttonClickHandler);
+
+function buttonClickHandler(event) {
+  const randomColor = getRandomHexColor();
+
+  colorRef.textContent = randomColor;
+  document.body.style.backgroundColor = randomColor;
+}
