2472b43a2721610b3c5c6fcdc3fe3d4bbb7d1834
Salnikova-Anna <anna_val@ukr.net>
2023-06-29 15:56:23 +0300

    Complete task-10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,43 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controlsRef = document.querySelector("#controls");
+const inputRef = controlsRef.firstElementChild;
+const createBtnRef = inputRef.nextElementSibling;
+const destroyBtnRef = controlsRef.lastElementChild;
+const boxesRef = document.querySelector("#boxes");
+
+createBtnRef.addEventListener("click", createBtnClickHandler);
+destroyBtnRef.addEventListener("click", destroyBtnClickHandler);
+
+function createBtnClickHandler() {
+  createBoxes(inputRef.value);
+}
+
+function destroyBtnClickHandler() {
+  destroyBoxes();
+}
+
+function createBoxes(amount) {
+  const boxesElementsArray = [];
+  let elementsSize = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    const boxesElement = document.createElement("div");
+
+    boxesElement.style.width = `${elementsSize}px`;
+    boxesElement.style.height = `${elementsSize}px`;
+    boxesElement.style.backgroundColor = getRandomHexColor();
+
+    boxesElementsArray.push(boxesElement);
+    elementsSize += 10;
+  }
+
+  boxesRef.append(...boxesElementsArray);
+}
+
+function destroyBoxes() {
+  boxesRef.innerHTML = "";
+  inputRef.value = "";
+}
