e9c91dd60f78e7fa30eb33b5e6220e41b2b570d6
Salnikova-Anna <anna_val@ukr.net>
2023-06-29 14:06:18 +0300

    Complete task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const loginFormRef = document.querySelector(".login-form");
 
+loginFormRef.addEventListener("submit", loginFormSubmitHandler);
+
+function loginFormSubmitHandler(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Всі поля форми повинні бути заповнені");
+  } else {
+    const formData = {
+      email: email.value,
+      password: password.value,
+    };
+
+    console.log(formData);
+
+    event.currentTarget.reset();
+  }
+}
